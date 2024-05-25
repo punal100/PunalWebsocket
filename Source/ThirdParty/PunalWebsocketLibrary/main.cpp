7b4855ee12626f1ad88f93f41c02da25885008bc
Punal Manalan <punal100@gmail.com>
2024-05-25 15:24:52 +0530

    CMake Changes For OpenSSL Compilation and Working Websocket SSL Example.
    Summary:
    CMake Changes For OpenSSL Compilation
    Working Websocket SSL Example.
    
    Changes:
    1. CMake Changes For OpenSSL Compilation.
    2. Working Websocket SSL Example.
    
    Tests:
    1. Good Enough.
    
    TODO:
    1. Make OpenSSL and Boost Linking/Building Modular.
    
    Update By Punal Manalan,
    
    Signed-off-by: Punal Manalan <punal100@gmail.com>

--- Source/ThirdParty/PunalWebsocketLibrary/main.cpp
+++ Source/ThirdParty/PunalWebsocketLibrary/main.cpp
@@ -37,11 +37,11 @@ int main(int argc, char** argv)
                 "Usage: websocket-client-sync-ssl <host> <port> <text>\n" <<
                 "Example:\n" <<
                 "    websocket-client-sync-ssl echo.websocket.org 443 \"Hello, world!\"\n";
-            return EXIT_FAILURE;
+            //return EXIT_FAILURE;
         }
-        auto const host = argv[1];
-        auto const port = argv[2];
-        auto const text = argv[3];
+        std::string host = "echo.websocket.org";
+        auto const  port = "443";
+        auto const  text = "Punal Echo Test";
 
         // The io_context is required for all I/O
         net::io_context ioc;
@@ -60,7 +60,20 @@ int main(int argc, char** argv)
         auto const results = resolver.resolve(host, port);
 
         // Make the connection on the IP address we get from a lookup
-        net::connect(ws.next_layer().next_layer(), results.begin(), results.end());
+        auto ep = net::connect(get_lowest_layer(ws), results);
+
+        // Set SNI Hostname (many hosts need this to handshake successfully)
+        if (!SSL_set_tlsext_host_name(ws.next_layer().native_handle(), host.c_str()))
+            throw beast::system_error(
+                beast::error_code(
+                    static_cast<int>(::ERR_get_error()),
+                    net::error::get_ssl_category()),
+                "Failed to set SNI Hostname");
+
+        // Update the host_ string. This will provide the value of the
+        // Host HTTP header during the WebSocket handshake.
+        // See https://tools.ietf.org/html/rfc7230#section-5.4
+        host += ':' + std::to_string(ep.port());
 
         // Perform the SSL handshake
         ws.next_layer().handshake(ssl::stream_base::client);
@@ -71,7 +84,7 @@ int main(int argc, char** argv)
             {
                 req.set(http::field::user_agent,
                 std::string(BOOST_BEAST_VERSION_STRING) +
-                " websocket-client-coro");
+                " websocket-client-cor");
             }));
 
         // Perform the websocket handshake
@@ -84,6 +97,7 @@ int main(int argc, char** argv)
         beast::flat_buffer buffer;
 
         // Read a message into our buffer
+        ws.read(buffer);
         ws.read(buffer);
 
         // Close the WebSocket connection
